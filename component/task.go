f88287ee9c998bdb3bcb626bc1ef9f195a53f043
Mitchell Hashimoto <mitchell.hashimoto@gmail.com>
2021-08-24 11:36:20 -0700

    Add entrypoint to the TaskLaunchInfo

--- component/task.go
+++ component/task.go
@@ -13,9 +13,12 @@ type TaskLaunchInfo struct {
 	// for the task to see when it starts.
 	EnvironmentVariables map[string]string
 
+	// Entrypoint is the entrypoint override for this image. If this is not
+	// set, then the default entrypoint for the OCI image should be used.
+	Entrypoint []string
+
 	// Arguments is passed as command line arguments to the image when it started.
 	// If the image defines an entrypoint, then the arguments will be passed as
-	// arguments to that program. Otherwise, arguments should contain the program
-	// to run inside the image as the first value in Arguments.
+	// arguments to that program.
 	Arguments []string
 }
